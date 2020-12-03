ef61b4bb014cc841f5028ac8b2a0e3827a552f06
xiaolinyang927 <xyang62@stevens.edu>
2020-12-03 15:11:22 -0500

    user login

--- data/index.js
+++ data/index.js
@@ -1,9 +1,9 @@
-// const userData = require('./users');
+const usersData = require('./users');
 const attractions = require('./attractions');
 // const reviewData = require('./reviews');
 // const commentData = require('./comments');
 module.exports = { 
-    // users: userData,
+    users: usersData,
     attractions: attractions,
     // reviews: reviewData,
     // comments: commentData
