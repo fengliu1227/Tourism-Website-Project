8db9473fe6cd4a83b2e7c2ec506c004f726fdd22
fengliu1227 <fengliu1227@outlook.com>
2020-12-16 18:55:42 -0500

    update comment

--- data/index.js
+++ data/index.js
@@ -17,26 +17,37 @@ module.exports = {
     adminDeleteInfo: adminDeleteInfo
 };
 
+const main = async() => {
+        await comments.updateComment("5fd9c22d3a9948f723c76c23", "3", "changed");
+    }
+    // main();
+    /* user data format:
+        userName: {firstName: 'N/A', lastName: 'N/A'},
+        email: email,
+        gender: 'N/A',
+        password: hashedPassword,
+        privilege: 0,
+        commentId: [],
+        spotsId: [],
+        travelogueId: [] 
 
+    attaction data format:
+        userId : userId,
+        relatedCommentsId : relatedCommentsId,
+        relatedTravelogueId : relatedCommentsId,
+        description :{Name,Category,Rating,Img,Price,Content,Address}
 
-/* user data format:
-    userName: {firstName: 'N/A', lastName: 'N/A'},
-    email: email,
-    gender: 'N/A',
-    password: hashedPassword,
-    privilege: 0,
-    commentId: [],
-    spotsId: [],
-    travelogueId: [] 
+    travelogue data format:
+        userId: userId,
+        relatedAttractoinId: relatedAttractoinId,
+        travelogue: {title, content}
 
-attaction data format:
-    userId : userId,
-    relatedCommentsId : relatedCommentsId,
-    relatedTravelogueId : relatedCommentsId,
-    description :{Name,Category,Rating,Img,Price,Content,Address}
-
-travelogue data format:
-    userId: userId,
-    relatedAttractoinId: relatedAttractoinId,
-    travelogue: {title, content}
-*/
+    comment data format:
+            userId: userId.toString(),
+            relatedAttractionId: relatedAttractionId.toString(),
+            like: 0,
+            unlike: 0,
+            rating: rating,
+            comment: comment
+        };
+    */
