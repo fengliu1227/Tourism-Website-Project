ca414792768a6a85e5724ce82e58c77615e02a20
fengliu1227 <fengliu1227@outlook.com>
2020-12-17 00:45:20 -0500

    one user only can comment once

--- routes/comments.js
+++ routes/comments.js
@@ -3,37 +3,7 @@ const router = express.Router();
 const data = require('../data');
 const comments = data.comments;
 
-// router.get('/private', async (req, res) => {
-//     if (req.session.user) {
-//         let userId = req.session.user.userId;
-//         let result = await comments.getCommentByUserId(userId);
-//         if(result){
-//             res.render('comments/private', {result, noEmpty: true});
-//         }else{
-//             res.render('comments/private', {empty: true});
-//         }
-//     }else {
-//         res.redirect('/users/login')
-//     }
-// });
 
-// router.post('/private/comments', async (req, res) => {
-//     if (req.session.user) {
-//         let user = req.session.user;
-//         let userId = user.userId;
-//         let attricationId = req.attricationId;
-//         let description = req.description;
-
-//         let result = await comments.addComments(userId, attricationId, description);
-//         if(result){
-//             res.render('comments/addComment', {success: true});
-//         }else{
-//             res.render('comments/addComment', {error: true});
-//         }
-//     }else {
-//         res.redirect('/users/login')
-//     }
-// });
 router.get('/update/:id', async(req, res) => {
     if (req.session.user) {
         res.render('comments/updateComment');
@@ -47,7 +17,7 @@ router.post('/update/:id', async(req, res) => {
         if (req.body.changeWhich == "ratingOnly") {
             try {
                 const oldComment = await comments.getCommentsById(req.params.id);
-                const comment = await comments.updateComment(req.params.id, req.body.updatedRating, oldComment.comment);
+                await comments.updateComment(req.params.id, req.body.updatedRating, oldComment.comment);
                 res.render('comments/updateComment', {
                     sucuess: true
                 });
