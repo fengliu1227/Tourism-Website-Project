12a55890de0e1697b617a64792ed13c21b1a0827
hungrylz <zliu110@stevens.edu>
2020-12-17 18:05:31 -0500

    data and seeds

--- tasks/seed.js
+++ tasks/seed.js
@@ -10,16 +10,28 @@ const main = async () => {
     const db = await dbConnection();
     await db.dropDatabase();
     await users.createUser("admin@outlook.com","12345","admin","admin");
+    console.log("tests");
     await admin.createAdmin("admin@outlook.com");
+    console.log("test34");
     console.log("admin"+ "finished!");
     const userList = [];
     console.log("There are 10 users initialization!")
-    for(let i = 1; i < 10; i++){
+    for(let i = 0; i < 10; i++){
      userList[i] = await users.createUser("user"+i+"@outlook.com","12345","user"+i,"user"+i);
      console.log("user"+i+" finished!");
     }
     const descriptions = [];
-    
+    descriptions.push({
+        Name:"Eiffel Tower",
+        Category:"Culture",
+        Rating:"5",
+        Img:"https://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg",
+        Price:"25",
+        Content:"The most visited tourist attraction in Paris, the Eiffel Tower also ranks high on the list of places to visit in France. It's hard to believe that the structure was dismissed as a monstrosity when it was first unveiled.",
+        Address:"Paris"
+    });
+    console.log(userList[0]);
+    await attraction.addAttractions(userList[0]._id,descriptions[0]);
 
 
 
